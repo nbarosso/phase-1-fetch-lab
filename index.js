5cbc48e7c19da2f51005f873d22c8b32faf18554
Nicholas Barosso <nbarosso@gmail.com>
2023-09-03 12:58:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function fetchBooks() {
+// function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+// }
+
+function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
+    return fetch("https://anapioficeandfire.com/api/books")
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
